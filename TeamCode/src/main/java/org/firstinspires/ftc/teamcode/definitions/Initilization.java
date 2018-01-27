0ea5ca58e8585368829a95883616403cd19a2c82
EliSauder <elijah.sauder@gmail.com>
2018-01-26 19:54:27 -0800

    Initilized motors in Initilization.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/definitions/Initilization.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/definitions/Initilization.java
@@ -1,8 +1,19 @@
 package org.firstinspires.ftc.teamcode.definitions;
 
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
 /**
  * Created by Elijah Sauder for Bambusa in Robot-Controller_17-18, on 01/26/2018.7:48 PM.
  */
 
 public class Initilization {
+
+    public DcMotorEx driveFrontRight = null;
+    public DcMotorEx driveFrontLeft = null;
+    public DcMotorEx driveBackRight = null;
+    public DcMotorEx driveBackLeft = null;
+
+    public DcMotorEx lift = null;
+
+    public DcMotorEx relicGrabRot = null;
 }

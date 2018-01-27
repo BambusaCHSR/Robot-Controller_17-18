f23797a0c4f5047e6e50db19ef055f36ecd35b72
EliSauder <elijah.sauder@gmail.com>
2018-01-26 20:02:58 -0800

    Initilized servos and finished initilizing motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/definitions/Initilization.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/definitions/Initilization.java
@@ -1,6 +1,13 @@
 package org.firstinspires.ftc.teamcode.definitions;
 
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Hardware;
+import com.vuforia.Vuforia;
+
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 
 /**
  * Created by Elijah Sauder for Bambusa in Robot-Controller_17-18, on 01/26/2018.7:48 PM.
@@ -16,4 +23,32 @@ public class Initilization {
     public DcMotorEx lift = null;
 
     public DcMotorEx relicGrabRot = null;
+
+    public Servo glyphGrabRight = null;
+    public Servo glyphGrabLeft = null;
+
+    public Servo jewelKnocker = null;
+    public Servo relicGrab = null;
+
+    public ColorSensor jewelColor = null;
+    public ColorSensor teamColor = null;
+
+    public VuforiaLocalizer vuforia = null;
+
+
+    private int cameraMonitorViewId;
+
+    public void hardwareMapInit(HardwareMap Map) {
+        driveFrontRight = (DcMotorEx)Map.dcMotor.get("driveFrontRight");
+        driveFrontLeft = (DcMotorEx)Map.dcMotor.get("driveFrontLeft");
+        driveBackRight = (DcMotorEx)Map.dcMotor.get("driveBackRight");
+        driveBackLeft = (DcMotorEx)Map.dcMotor.get("driveBackLeft");
+
+        lift = (DcMotorEx)Map.dcMotor.get("lift"); //make sure to change robot config from winch to lift
+
+        glyphGrabRight = Map.servo.get("glyphGrabRight");
+        glyphGrabLeft = Map.servo.get("glyphGrabLeft");
+
+        jewelKnocker = Map.servo.get("jewelKnocker"); //make sure to change robot config from jewels to jewelKnocker
+    }
 }

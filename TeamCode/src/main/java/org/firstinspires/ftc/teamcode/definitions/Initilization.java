f2faf35db8ac22f2f9de531ac6798fa582b3c43a
EliSauder <elijah.sauder@gmail.com>
2018-01-26 20:13:26 -0800

    Added more initilization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/definitions/Initilization.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/definitions/Initilization.java
@@ -1,12 +1,14 @@
 package org.firstinspires.ftc.teamcode.definitions;
 
 import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Hardware;
 import com.vuforia.Vuforia;
 
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 
 /**
@@ -15,14 +17,13 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 
 public class Initilization {
 
-    public DcMotorEx driveFrontRight = null;
-    public DcMotorEx driveFrontLeft = null;
-    public DcMotorEx driveBackRight = null;
-    public DcMotorEx driveBackLeft = null;
+    public DcMotor driveFrontRight = null;
+    public DcMotor driveFrontLeft = null;
+    public DcMotor driveBackRight = null;
+    public DcMotor driveBackLeft = null;
 
-    public DcMotorEx lift = null;
-
-    public DcMotorEx relicGrabRot = null;
+    public DcMotor lift = null;
+    public DcMotor relicGrabRot = null;
 
     public Servo glyphGrabRight = null;
     public Servo glyphGrabLeft = null;
@@ -39,16 +40,42 @@ public class Initilization {
     private int cameraMonitorViewId;
 
     public void hardwareMapInit(HardwareMap Map) {
-        driveFrontRight = (DcMotorEx)Map.dcMotor.get("driveFrontRight");
-        driveFrontLeft = (DcMotorEx)Map.dcMotor.get("driveFrontLeft");
-        driveBackRight = (DcMotorEx)Map.dcMotor.get("driveBackRight");
-        driveBackLeft = (DcMotorEx)Map.dcMotor.get("driveBackLeft");
+        driveFrontRight = Map.dcMotor.get("driveFrontRight");
+        driveFrontLeft = Map.dcMotor.get("driveFrontLeft");
+        driveBackRight = Map.dcMotor.get("driveBackRight");
+        driveBackLeft = Map.dcMotor.get("driveBackLeft");
 
-        lift = (DcMotorEx)Map.dcMotor.get("lift"); //make sure to change robot config from winch to lift
+        lift = Map.dcMotor.get("lift"); //make sure to change robot config from winch to lift
+        relicGrabRot = Map.dcMotor.get("relicGrabRot");
 
         glyphGrabRight = Map.servo.get("glyphGrabRight");
         glyphGrabLeft = Map.servo.get("glyphGrabLeft");
 
         jewelKnocker = Map.servo.get("jewelKnocker"); //make sure to change robot config from jewels to jewelKnocker
+
+        jewelColor = Map.colorSensor.get("jewelColor");
+
+        cameraMonitorViewId = Map.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", Map.appContext.getPackageName());
+    }
+
+    public void servoInit() {
+        glyphGrabRight.setPosition(1);
+        glyphGrabLeft.setPosition(0);
+
+        jewelKnocker.setPosition(0.96);
+    }
+
+    public void vuforiaInit() {
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+        parameters.vuforiaLicenseKey = "AS/00FX/////AAAAGRywycu7JEUJh4rz/rwORDkZJabbBKzxMuMKtBDf4H4wwDJtDTdfM4a0JXDJiPhDkK9SUSl9j8qDz8nbFunZwugJW7SdZvehZLe9J7Irk1Eu6w4+gmSEIvRBSxite8W+7xjTTtLyvMgxKbMacL8k0uuhfFJzwTS3qZ5FCm8HP7F8jqXgH5e2LLcRyJ+MYdNqPPx7wpISBVFY3rYGqxFJgAs1S1dReiziGOeyckCt1pcJXA6IroX5mgAVNwLWx9Wafoe3jd3dNdD+dxSep2E1MhmcA+WIcKIZ9615uMPfon5M7fyzjB6CFu5srJTpcgoVPbbr6LAZt7WiNPLLCuoFNx246V29fO1DeiTErTCbuL/Q";
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+    }
+
+    public void encoderInit() {
+        driveFrontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        driveFrontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        driveBackRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        driveBackLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 }
